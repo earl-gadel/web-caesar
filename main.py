9143c0f8925318875918e307b188520455278cbd
Earl <earljg@gmail.com>
2018-03-29 20:05:44 -0500

    fixed CSS with {{}} returned form.format to return text to text area

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +31,7 @@ form = """
                 Rotate by:
                 <input type="text" name="rot" value="0">
             </label>
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit" value="Submit Query">
         </form>
     </body>
@@ -40,7 +40,7 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format(...)
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -49,7 +49,7 @@ def encrypt():
 
 
     encrypted = rotate_string(text_field, rotation)
-    return "<h1>" + encrypted + "</h1>"
+    return "<h1>" + form.format(encrypted) + "</h1>"
 
 
 
